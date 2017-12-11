f495a9d7a434ed21b22f450490f2d10bf7b338ad
helinlin0717 <31975807+helinlin0717@users.noreply.github.com>
2017-12-11 13:21:58 +0800

    layout布局

---
+++ study/src/router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
+import IndexPage from './routes/IndexPage';
+import BasicLayout from './layout/BasicLayout';
+import Out from './layout/out';
+
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/a" component={IndexPage} />
+        <Route path="/" component={BasicLayout} />
+        <Route path="/b" component={Out} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default RouterConfig;
